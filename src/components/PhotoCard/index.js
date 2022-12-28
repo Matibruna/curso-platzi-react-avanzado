40ec502ea89d8474b4b069e87a3e6c39a73afaf9
Matias Bruna <matibruna6@gmail.com>
2022-12-28 15:15:07 -0300

    Clase 20

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,53 +1,25 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { ImgWrapper, Image, Button, Article } from './styles'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE }) => {
 
-    const ref = useRef(null)
-    const [show, setShow] = useState(false)
-
     const key = `like-${id}`
 
-    const [liked, setLiked] = useState(function(){
-        try{
-            const like = window.localStorage.getItem(key)
-            return like
-        }catch(e){
-            console.log(e)
-            return false
-        }
-    })
+    const [liked, setLiked] = useLocalStorage(key, false)
 
-    useEffect(() => {
-        const observer = new window.IntersectionObserver(function (entries){
-            const { isIntersecting } = entries[0]
-            if(isIntersecting){
-                setShow(true)
-                observer.disconnect()
-            }
-        })
-        observer.observe(ref.current)
-    }, [ref])
+    const [show, ref] = useNearScreen();
 
     const Icon = liked ? MdFavorite : MdFavoriteBorder
-
-    const setLocalStorage = value => {
-        try{
-            window.localStorage.setItem(key, value)
-            setLiked(value)
-        }catch(e){
-            console.error(e)
-        }
-    }
 
     return(
         <Article ref={ref}>
             {
                 show ? 
-
                 <>
                     <a href={`/detail/${id}`}>
                         <ImgWrapper>
@@ -55,7 +27,7 @@ export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE }) => {
                         </ImgWrapper>
                     </a>
 
-                    <Button onClick={() => setLocalStorage(!liked)}>
+                    <Button onClick={() => setLiked(!liked)}>
                         <Icon size='32px' /> {likes} likes!
                     </Button>
                 </>
