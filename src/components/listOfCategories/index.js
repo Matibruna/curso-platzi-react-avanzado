a2bac114f4e0bf716d4cd22f14acb49d942d2dd7
Matias Bruna <matibruna6@gmail.com>
2022-12-27 22:50:36 -0300

    Clase 14

--- src/components/listOfCategories/index.js
+++ src/components/listOfCategories/index.js
@@ -1,14 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Category } from "../Category"
 import { List, Item } from "./styles"
-import db from "../../../api/db.json"
 
 export const ListOfCategories = () => {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(function (){
+        fetch('https://petgram-server-matiasb-matibruna.vercel.app/categories')
+            .then( res => res.json() )
+            .then( response => {
+                setCategories(response)
+            })
+    }, [])
 
     return(
         <List>
             {
-                db.categories.map( category => 
+                categories.map( category => 
                         <Item key={category.id}>
                             <Category {...category}/>
                         </Item>
