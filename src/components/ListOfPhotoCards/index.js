0c169157831967a9997085e26144d60b46052964
Matias Bruna <matibruna6@gmail.com>
2022-12-28 15:49:51 -0300

    Clase 21

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { useQuery, gql } from '@apollo/client'
 
-export const ListOfPhotoCards = () => {
+const ANIMALS_QUERY = gql`
+    query getPhotos {
+        photos {
+            id
+            categoryId
+            src
+            likes
+            userId
+            liked
+        }
+    }
+`
+
+const ListOfPhotoCardsComponent = () => {
 
     return(
         <ul>
@@ -11,3 +25,14 @@ export const ListOfPhotoCards = () => {
         </ul>
     )
 }
+
+export const ListOfPhotoCards = () => {
+    const { data, loading, error } = useQuery(ANIMALS_QUERY)  // destructuras la data y el estado de loading y error 
+    if (loading) return 'Loading...' // manejas el estado para que no te saque error mientras hace el fetch
+    if (error) return <pre>{error.message}</pre>
+    return (
+      <ul>
+        {data.photos.map(photo => <PhotoCard key={photo.id} {...photo} />)}
+      </ul>
+    )
+  }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+// APP
 import App from './App'
+
+const client = new ApolloClient({
+  uri: 'https://petgram-server-matiasb-matibruna.vercel.app/graphql',
+  cache: new InMemoryCache(),
+})
 
 const root = ReactDOM.createRoot(document.getElementById('app'))
 
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
 )
